b3b69763c4cb10e59ab4a9c47fe8b5186d4c7dc7
Leonardo Sjöberg <leonardo01@hotmail.se>
2024-09-10 15:15:08 +0200

    First commit

---
+++ Kunskapskontroll.py
@@ -0,0 +1,14 @@
+# venv Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
+# venv .venv\Scripts\activate
+
+import logging
+import sqlalchemy
+import pandas
+import pytest
+
+# ladda in csv data
+
+
+# bearbeta vikktiga värden
+
+# Skicka in i SQL
